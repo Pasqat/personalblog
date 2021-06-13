7e3071a6e7cd75996b9a8a70e8aea77f9a0d483d
Pasquale Matarrese <pasquale.matarrese@gmail.com>
2021-06-13 15:37:04 +0200

    fetch data from sanity

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,41 @@
 import { Row, Col } from 'react-bootstrap';
 
+import { getAllBlogs } from 'lib/api';
+
 import PageLayout from 'components/PageLayout';
 import AuthorIntro from 'components/AuthorIntro';
 import CardItem from 'components/CardItem';
 import CardListItem from 'components/CardListItem';
 
-export default function Home() {
+export default function Home({ blogs }) {
+  debugger;
   return (
     <PageLayout>
       <AuthorIntro />
       <hr />
       <Row className="mb-5">
+        {/* 
         <Col md="10">
-          <CardListItem />
+        <CardListItem />
         </Col>
-
-        <Col md="4">
-          <CardItem />
-        </Col>
+        */}
+        {blogs.map((blog) => (
+          <Col md="4">
+            <CardItem title={blog.title} subtitle={blog.subtitle} />
+          </Col>
+        ))}
       </Row>
     </PageLayout>
   );
+}
+
+// this function is called only during build time (server side)
+// Provide props to your page, used to creat a `static page`
+export async function getStaticProps() {
+  const blogs = await getAllBlogs();
+  return {
+    props: {
+      blogs,
+    },
+  };
 }
